703d3e9cbdee3e6ed2896b51d0f1a1161315f442
Cheikh melainine <Cheikhmelaininehaimidaha@gmail.com>
2023-07-05 01:42:09 +0200

    Ajoutez des fichiers projet.

---
+++ Courses/Areas/Instracter/InstracterAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Courses.Areas.Instracter
+{
+    public class InstracterAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Instracter";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Instracter_default",
+                "Instracter/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Courses/Models/CourseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Courses.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoruId { get; set; }
+
+        public Category category { get; set; }
+    }
+}
