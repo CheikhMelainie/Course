e5c1b93e8f863621305f51ef332eb8862dfcc3a9
Cheikh melainine <Cheikhmelaininehaimidaha@gmail.com>
2023-07-13 16:33:52 +0200

    Dashoard Update

--- Courses/Areas/Admin/Controllers/DefaultController.cs
+++ Courses/Areas/Admin/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Courses.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,23 @@ namespace Courses.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class DefaultController : Controller
     {
+        Courses_DBEntities1 context = new Courses_DBEntities1();
+
         // GET: Admin/Default
         public ActionResult Index()
         {
+            int nbrCours = context.Courses.Count();
+            ViewBag.nbrCourse = nbrCours;
+
+            int nbrTrainer = context.Trainees.Count();
+            ViewBag.nbrTrainer = nbrTrainer;
+
+            int nbrTrainee = context.Trainees.Count();
+            ViewBag.Trainee = nbrTrainee;
+
+            int nbrCategory = context.Categories.Count();
+            ViewBag.Categiry = nbrCategory;
+
             return View();
         }
     }
